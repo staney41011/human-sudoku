1fa545586d7374d9846848dc25562a3dd1dff3c3
staney41011 <45749474+staney41011@users.noreply.github.com>
2026-09-07 23:04:19 +0800

    Add Firebase configuration template

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+export const firebaseConfig = {
+  apiKey: "PASTE_YOUR_API_KEY",
+  authDomain: "PASTE_YOUR_PROJECT.firebaseapp.com",
+  databaseURL: "https://PASTE_YOUR_PROJECT-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "PASTE_YOUR_PROJECT",
+  storageBucket: "PASTE_YOUR_PROJECT.appspot.com",
+  messagingSenderId: "PASTE_YOUR_SENDER_ID",
+  appId: "PASTE_YOUR_APP_ID"
+};
+
+export const APP_NAME = "無獨有偶，有你有偶";
+export const RECOVERY_TTL_MS = 5 * 60 * 1000;
